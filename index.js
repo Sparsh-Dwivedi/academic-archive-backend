9ec63e63089a08308f62bb877a2593aaacb0a8d9
yashjaiswal-only <dspvnkras12102@gmail.com>
2023-09-11 11:39:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const authRoute=require("./routes/auth")
+
+const cors= require("cors");
+const dotenv=require("dotenv");
+dotenv.config();
+const app=express();     
+app.use(cors());   
+
+//  console.log(process.env.MONGO_URL);
+mongoose.set("strictQuery", true); //to eliminate a error
+mongoose
+.connect(process.env.MONGO_URL) // we can also use try catch
+.then(()=>console.log("db connection successfull")) //if promise return true
+.catch((err)=>{console.log(err)});  //if promise return false
+
+app.use(express.json());  
+
+app.use("/api/auth",authRoute);
+
+
+app.get('/',(req,res)=>{
+    res.send('hii this is api');
+}) 
+
+
+
+
+//this builds express server in port 5000 
+app.listen(process.env.PORT || 5000,()=>{
+    console.log("backend server is running");
+})
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+
+const UserSchema=new mongoose.Schema(
+    {
+        username:{type:String,required:true,unique:true},
+        email:{type:String},
+        name:{type:String,default:'User'},
+        // address:{type:String,default:'INDIA'},
+        qualification:{type:String},
+        ph:{type:Number},
+        password:{type:String,required:true},
+        avatar:{type:String,default:'https://www.pngarts.com/files/10/Default-Profile-Picture-PNG-Free-Download.png'},
+        isAdmin:{
+            type:Boolean,
+            default:false,
+        },
+        //mongoose has defalut option for created date time
+    },
+    {timestamps:true}
+);
+
+module.exports=mongoose.model("User",UserSchema);
+
+//this is how we can create an model 
